8d21dd5d782e6a8d8bf8d49815c01f22bc5a2df0
Neliane de Oliveira <casacondominioclube@gmail.com>
2022-07-10 20:19:53 -0300

    Test java

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Object livros = Livros;
+		System.out.println(gato);
+		// TODO Auto-generated method 
+	/*	int a = 2;
+		int b = 3;
+		System.out.println("Hello Word!" + (a+b));*/
+	}
+
+}
+ class Livros {
+	 private String nome;
+	 private String npag;
+ }
+ 
---
+++ src/com/dio/model/Gato.java
@@ -0,0 +1,50 @@
+package com.dio.model;
+
+public class Gato {
+	private String nome;
+	private String cor;
+	private Integer idade;
+	public Gato(String nome, String cor, Integer idade) {
+		super();
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	@Override
+	public String toString() {
+		return "Gato [nome=" + nome + ", cor=" + cor + ", idade=" + idade + ", getNome()=" + getNome() + ", getCor()="
+				+ getCor() + ", getIdade()=" + getIdade() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
+	
+	/*public Gato(String nome, String cor, Integer idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}*/
+	
+	
+	
+	
+
+}
