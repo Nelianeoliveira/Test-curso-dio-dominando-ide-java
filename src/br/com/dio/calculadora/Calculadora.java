c8b66a443cfaf5c5a0eb3b71b0b8a6fac93e7c40
Neliane de Oliveira <casacondominioclube@gmail.com>
2022-07-10 21:49:21 -0300

    Calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,46 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan = new Scanner(System.in); 
+int a, b;
+System.out.println(" Digite o primeiro valor:");
+a = scan.nextInt();
+System.out.println("Digite o segundo valo:r");
+b = scan.nextInt();
+int soma = soma(a,b);
+int subtracao = subtracao(a,b);
+int divisao= divisao(a,b);
+int multiplicacao = multiplicacao(a,b);
+
+System.out.println("Soma: " + soma);
+System.out.println("sub: " + subtracao);
+System.out.println("div: " + divisao);
+System.out.println("mult: " + multiplicacao);
+
+
+	}
+	
+ public static int soma(int a, int b) {
+	 return a + b;
+	 
+ }
+ public static int subtracao(int a, int b) {
+	 return a - b;
+	 
+ }
+
+public static int divisao(int a, int b) {
+	return a / b;
+	
+}
+
+public static int multiplicacao(int a, int b) {
+	return a * b;
+	
+}
+}
